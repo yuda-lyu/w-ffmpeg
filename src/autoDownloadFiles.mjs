e3c71fe1258434131c51e929907ac09ccae8afdd
yuda-lyu <firsemisphere@gmail.com>
2026-09-03 14:13:26 +0800

    modify: autoDownloadFiles

--- src/autoDownloadFiles.mjs
+++ src/autoDownloadFiles.mjs
@@ -1,13 +1,22 @@
 import path from 'path'
 import fsIsFile from 'wsemi/src/fsIsFile.mjs'
+import fsIsFolder from 'wsemi/src/fsIsFolder.mjs'
 import downloadFiles from './downloadFiles.mjs'
+
+
+//fnExe, ffmpeg執行檔名稱
+let fnExe = 'ffmpeg.exe'
+
+
+//pmDownload, 下載中之Promise, 供併發呼叫共用同一次下載, 避免同時多次下載寫入同一批檔案
+let pmDownload = null
 
 
 /**
  * 自動定位ffmpeg.exe，若無檔案則自動下載，回傳ffmpeg.exe的絕對路徑
  *
  * 依序偵測當前工作路徑的src/與node_modules/w-ffmpeg/src/，皆無ffmpeg.exe時，
- * 代表安裝時npm封鎖scripts致postinstall未執行，故自動調用downloadFiles重新下載，落點為node_modules/w-ffmpeg/src/
+ * 代表安裝時npm封鎖scripts致postinstall未執行，故自動調用downloadFiles重新下載
  *
  * 供w-ffmpeg自身與其他依賴w-ffmpeg的套件(例如w-dwload-dlp)調用，無須各自實作偵測與下載邏輯
  *
@@ -36,39 +45,51 @@ async function autoDownloadFiles() {
     //fdSrv, 於調用時取當前工作路徑
     let fdSrv = path.resolve()
 
-    //fnExe
-    let fnExe = 'ffmpeg.exe'
+    //fdBaseSelf, fdBaseNM, ffmpeg.exe可能所在資料夾(開發套件本身時於cwd的src/, 被安裝為相依套件時於node_modules/w-ffmpeg/src/)
+    let fdBaseSelf = `${fdSrv}/src/`
+    let fdBaseNM = `${fdSrv}/node_modules/w-ffmpeg/src/`
 
-    //fdExeSrc, fdExeNM, ffmpeg.exe可能所在資料夾(開發套件本身時於cwd的src/, 被安裝為相依套件時於node_modules/w-ffmpeg/src/)
-    let fdExeSrc = `${fdSrv}/src/`
-    let fdExeNM = `${fdSrv}/node_modules/w-ffmpeg/src/`
-
-    //fdExe
-    let fdExe = ''
-    if (fsIsFile(`${fdExeSrc}${fnExe}`)) {
-        fdExe = fdExeSrc
+    //fdBase
+    let fdBase = ''
+    if (fsIsFile(`${fdBaseSelf}${fnExe}`)) {
+        fdBase = fdBaseSelf
     }
-    else if (fsIsFile(`${fdExeNM}${fnExe}`)) {
-        fdExe = fdExeNM
+    else if (fsIsFile(`${fdBaseNM}${fnExe}`)) {
+        fdBase = fdBaseNM
     }
     else {
 
-        //downloadFiles, 無ffmpeg.exe代表安裝時npm封鎖scripts致postinstall未執行,
-        //故於此重新執行下載, 落點為本套件於node_modules內的src/
-        await downloadFiles(fdExeNM)
-        if (fsIsFile(`${fdExeNM}${fnExe}`)) {
-            fdExe = fdExeNM
+        //fdBaseDL, 下載落點, 有node_modules/w-ffmpeg/代表為被安裝之相依套件, 否則為套件自身
+        let fdBaseDL = fsIsFolder(`${fdSrv}/node_modules/w-ffmpeg/`) ? fdBaseNM : fdBaseSelf
+
+        //downloadFiles, 無ffmpeg.exe代表安裝時npm封鎖scripts致postinstall未執行, 故於此重新執行下載,
+        //併發呼叫共用同一個下載Promise, 避免重複下載
+        if (pmDownload === null) {
+            pmDownload = downloadFiles(fdBaseDL)
+                .catch((err) => {
+
+                    //下載失敗歸零, 使下次呼叫可重試下載
+                    pmDownload = null
+
+                    return Promise.reject(err)
+                })
+        }
+        await pmDownload
+
+        //check
+        if (fsIsFile(`${fdBaseDL}${fnExe}`)) {
+            fdBase = fdBaseDL
         }
 
     }
 
     //check
-    if (fdExe === '') {
+    if (fdBase === '') {
         return Promise.reject('can not find ffmpeg.exe')
     }
 
     //fpExe
-    let fpExe = path.resolve(fdExe, fnExe)
+    let fpExe = path.resolve(fdBase, fnExe)
 
     return fpExe
 }
